d21bd8c8db305afee3ebbcceec85e3b26f444ffa
mariobrusarosco <mariobrusarosco@hotmail.com>
2018-11-19 13:28:11 -0200

    Add future dependencies and start a plain Sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,6 @@
+const Sidebar = () => 
+  <aside>
+    sidebar
+  </aside>
+
+export default Sidebar
--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
+import { render } from 'react-dom'
+
+// Temporary Components while out app doesn't have a router system
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+
+// APP'S MAIN STYLES
+import './styles/main.styl'
+
 // Global behavior
 // Setting up 'HMR'
 if(module.hot) {
   module.hot.accept()
 }
 
-// APP'S MAIN STYLES
-import './styles/main.styl'
-// Main Component
-import './components/App'
+render(
+  <div>
+    <Header />
+    <Sidebar />
+  </div>, 
+  document.querySelector('#app')
+)
 
 
 console.log(`Environment: ${NODE_ENV.production ? 'production' : 'development'}`);
